6a836cb4033db3a8055bb114ddcbe3c9649ff42c
Estefanía <zotapianola@gmail.com>
2020-04-01 22:16:02 -0300

    nav con logo y menú de hamburguesa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <img className="logo" alt="logo" src='https://adaptativamente.cl/img/logos/logo-adaptativamente.png'/>
+        <FontAwesomeIcon id="burgerMenu" icon={faBars} />
+      </nav>
     </div>
   );
 }
